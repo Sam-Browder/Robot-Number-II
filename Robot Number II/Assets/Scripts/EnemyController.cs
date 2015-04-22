e718763393ab641e64e3531cd197e319e83ab991
unknown <harrissa@HARRISSA-W20.rose-hulman.edu>
2015-04-22 15:50:49 -0400

    added scripts

--- Robot Number II/Assets/Scripts/EnemyController.cs
+++ Robot Number II/Assets/Scripts/EnemyController.cs
@@ -12,6 +12,7 @@ public class EnemyController : MonoBehaviour, ICharacter {
 	//booleans
 	public bool grounded;
 	public bool canDoubleJump;
+	public bool canClimb;
 	public bool groundOnLeft;
 	public bool groundOnRight;
 	public bool noGroundLeft;
@@ -30,15 +31,15 @@ public class EnemyController : MonoBehaviour, ICharacter {
 	private float attackTimer = 0.0f;
 	public float attackSpeed;
 	public float jumpTime;
-
+	
 	public float visionDistance;
-
+	
 	private ICharacter player;
 	private float distanceToPlayer;
 	private float directionToPlayer;
 	private ProjSpawner projspawner;
-
-
+	
+	
 	
 	// Use this for initialization
 	void Start () {
@@ -47,37 +48,38 @@ public class EnemyController : MonoBehaviour, ICharacter {
 		rb2d = gameObject.GetComponent<Rigidbody2D> ();
 		anim = gameObject.GetComponent<Animator> ();
 		this.projspawner = gameObject.GetComponentInChildren<ProjSpawner> ();
+		this.canClimb = false;
 	}
 	
-
+	
 	void Update () {
 		anim.SetBool("Grounded", grounded);
 		anim.SetFloat ("Speed", Mathf.Abs(rb2d.velocity.x));
-
+		
 		GroundOnLeft ();
 		GroundOnRight ();
 		NoGroundLeft ();
 		NoGroundRight ();
 		StandinOnPlayer ();
-
+		
 		time += Time.deltaTime;
 		attackTimer += Time.deltaTime;
-
-
-
+		
+		
+		
 		if (this.transform.position.x < this.player.GetPosition ().x) {
 			this.directionToPlayer = -1;
 		} else {
 			this.directionToPlayer = 1;
 		}
-
+		
 		this.distanceToPlayer = Mathf.Abs( this.transform.position.x - this.player.GetPosition ().x);
-
-
+		
+		
 		if (health <= 0.0) {
 			Die();
 		}
-
+		
 		AI ();
 		UpdateSprite ();		
 	}
@@ -110,7 +112,7 @@ public class EnemyController : MonoBehaviour, ICharacter {
 			rb2d.velocity = new Vector2 (-maxSpeed, rb2d.velocity.y);
 		}
 	}
-
+	
 	void AI(){
 		
 		if (this.distanceToPlayer > this.visionDistance) {
@@ -128,9 +130,9 @@ public class EnemyController : MonoBehaviour, ICharacter {
 			}else{
 				this.direction = 1;
 			}
-
+			
 		}
-
+		
 		if (this.direction < 0 && this.groundOnLeft || this.direction<0 && this.noGroundLeft ) {
 			if(this.grounded){
 				this.numOfJumps = 1;
@@ -151,19 +153,19 @@ public class EnemyController : MonoBehaviour, ICharacter {
 			}
 		}
 	}
-
-
+	
+	
 	void Respawn(){
 		GameObject newEnemy = (GameObject) Instantiate(Resources.Load("Enemy"));
 		newEnemy.transform.position = new Vector3(2.4f,1.4f,0.0f);
 		Destroy (gameObject);
 		
 	}
-
+	
 	public void Die(){
 		Destroy (gameObject);
 	}
-
+	
 	void UpdateSprite(){
 		if (direction < -0.1f) {
 			direction = -1;
@@ -174,95 +176,103 @@ public class EnemyController : MonoBehaviour, ICharacter {
 			direction = 1;
 		}
 	}
-
-
-
+	
+	
+	
 	public void ApplyDamage(float damage){
 		this.health = this.health - damage;
 	}
-
+	
 	public void SetGrounded(bool grd){
 		this.grounded = grd;
+	}
+	
+	public void SetCanClimb(bool canClimb){
+		this.canClimb = canClimb;
 	}
 	
 	public int GetDirection(){
 		return this.direction;
 	}
-
+	
 	public string GetTag(){
 		return this.tag.ToString();
 	}
-
+	
 	public Vector3 GetPosition(){
 		return this.transform.position;
 	}
-
+	
+	public bool GetCanClimb() {
+		return false;
+	}
+	
 	void Jump(){
 		if (this.numOfJumps>0 && this.jumpTime < this.time-.3){
 			rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
 			rb2d.AddForce(Vector2.up * jumpPower);	
 			this.numOfJumps= this.numOfJumps-1;
 			this.jumpTime= this.time;
-
-			}			
+			
+		}			
 	}
-
-
-
+	
+	
+	
 	void GroundOnLeft(){
 		Vector3 currentPos = this.transform.position;
 		Vector3 endPos = new Vector3 (currentPos.x - 1, currentPos.y-.3f, currentPos.z);
 		Debug.DrawLine(currentPos, endPos,Color.green);
 		this.groundOnLeft = Physics2D.Linecast (currentPos, endPos, 1 << LayerMask.NameToLayer ("Ground"));
 	}
-
+	
 	void GroundOnRight(){
 		Vector3 currentPos = this.transform.position;
 		Vector3 endPos = new Vector3 (currentPos.x + 1, currentPos.y-.3f, currentPos.z);
 		Debug.DrawLine(currentPos, endPos,Color.green);
 		this.groundOnRight = Physics2D.Linecast (currentPos, endPos, 1 << LayerMask.NameToLayer ("Ground"));
 	}
-
-
+	
+	
 	void NoGroundLeft(){
 		Vector3 currentPos = new Vector3(this.transform.position.x-.5f,this.transform.position.y, this.transform.position.z);
 		Vector3 endPos = new Vector3 (currentPos.x, currentPos.y-3f, currentPos.z);
 		Debug.DrawLine(currentPos, endPos,Color.green);
 		this.noGroundLeft = !Physics2D.Linecast (currentPos, endPos, 1 << LayerMask.NameToLayer ("Ground"));
 	}
-
+	
 	void NoGroundRight(){
 		Vector3 currentPos = new Vector3(this.transform.position.x+.5f,this.transform.position.y, this.transform.position.z);
 		Vector3 endPos = new Vector3 (currentPos.x, currentPos.y-3f, currentPos.z);
 		Debug.DrawLine(currentPos, endPos,Color.green);
 		this.noGroundRight = !Physics2D.Linecast (currentPos, endPos, 1 << LayerMask.NameToLayer ("Ground"));
 	}
-
+	
 	void StandinOnPlayer(){
 		Vector3 currentPos = this.transform.position;
 		Vector3 endPos = new Vector3 (currentPos.x, currentPos.y-1, currentPos.z);
 		Debug.DrawLine(currentPos, endPos,Color.green);
 		this.standingOnPlayer = Physics2D.Linecast (currentPos, endPos, 1 << LayerMask.NameToLayer ("Player"));
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
--- Robot Number II/Assets/Scripts/Player.cs
+++ Robot Number II/Assets/Scripts/Player.cs
@@ -2,30 +2,32 @@
 using System.Collections;
 
 public class Player : MonoBehaviour, ICharacter {
-
+	
 	//floats
 	public float maxSpeed = 3;
 	public float speed = 50f;
 	public float jumpPower = 200f;
-
+	public float climbPower = 1;
+	
 	//booleans
 	public bool grounded;
 	public bool canDoubleJump;
-
+	public bool canClimb;
+	
 	public int direction = 1;
-
+	
 	//refrences
 	private Rigidbody2D rb2d;
 	private Animator anim;
 	private ProjSpawner projspawner;
-
+	
 	//Game values
 	private float health =100.0f;
 	
-
+	
 	// Use this for initialization
 	void Start () {
-
+		
 		rb2d = gameObject.GetComponent<Rigidbody2D> ();
 		anim = gameObject.GetComponent<Animator> ();
 		this.projspawner = gameObject.GetComponentInChildren<ProjSpawner> ();
@@ -34,16 +36,15 @@ public class Player : MonoBehaviour, ICharacter {
 	// Update is called once per frame
 	//public Transform testProj;
 	void Update () {
-	
-		anim.SetBool("Grounded", grounded);
-		anim.SetFloat ("Speed", Mathf.Abs(rb2d.velocity.x));
-
+		
+		anim.SetBool ("Grounded", grounded);
+		anim.SetFloat ("Speed", Mathf.Abs (rb2d.velocity.x));
+		
 		if (health <= 0.0) {
-			Die();
+			Die ();
 		}
-
+		
 		UpdateSprite ();
-
 		if (Input.GetButtonDown ("Jump")){
 			Jump();		
 		}
@@ -54,7 +55,7 @@ public class Player : MonoBehaviour, ICharacter {
 			this.projspawner.ShootProjTwo();
 		}
 	}
-
+	
 	// do all physics in here
 	void FixedUpdate()
 	{
@@ -62,17 +63,16 @@ public class Player : MonoBehaviour, ICharacter {
 		easeVelocity.y = rb2d.velocity.y;
 		easeVelocity.z = 0.0f;
 		easeVelocity.x *= 0.80f;
-
+		Physics2D.IgnoreLayerCollision (LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Ground"), canClimb && ((Input.GetAxis("Vertical") > 0.1f) || (Input.GetAxis("Vertical") < -0.1f)) );
 		//fake friction
 		if (grounded) {
 			rb2d.velocity = easeVelocity;
 		}
-
+		
 		//moves player
 		float h = Input.GetAxis ("Horizontal");
 		rb2d.AddForce ((Vector2.right * speed) * h);
-
-
+		
 		//limits the speed
 		if (rb2d.velocity.x > maxSpeed) {
 			rb2d.velocity = new Vector2 (maxSpeed, rb2d.velocity.y);
@@ -80,9 +80,16 @@ public class Player : MonoBehaviour, ICharacter {
 		if (rb2d.velocity.x < -maxSpeed) {
 			rb2d.velocity = new Vector2 (-maxSpeed, rb2d.velocity.y);
 		}
+		
+		float v = Input.GetAxis ("Vertical");
+		Climb ();
+		
+		if (!canClimb) {
+			rb2d.gravityScale = 1;
+		}
 	}
-
-
+	
+	
 	void Jump(){
 		if (grounded){
 			rb2d.AddForce(Vector2.up * jumpPower);	
@@ -92,11 +99,18 @@ public class Player : MonoBehaviour, ICharacter {
 			if (canDoubleJump){
 				canDoubleJump = false;
 				rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
-				rb2d.AddForce(Vector2.up * jumpPower);
+				rb2d.AddForce(Vector2.up * jumpPower * Input.GetAxis("Horizontal"));
 			}			
 		}
 	}
-
+	
+	void Climb(){
+		if (canClimb) {
+			rb2d.gravityScale = 0;
+			rb2d.velocity = new Vector2 (0, 5 * Input.GetAxis ("Vertical"));
+		}
+	}
+	
 	void UpdateSprite(){
 		if (Input.GetAxis ("Horizontal") < -0.1f) {
 			direction = -1;
@@ -110,32 +124,40 @@ public class Player : MonoBehaviour, ICharacter {
 	void Respawn(){
 		Application.LoadLevel(Application.loadedLevel);
 	}
-
+	
 	public void Die(){
 		Application.LoadLevel(Application.loadedLevel);
 	}
-
+	
 	public void ApplyDamage(float damage){
 		this.health = this.health - damage;
 	}
-
+	
+	public void SetCanClimb(bool canClmb){
+		this.canClimb = canClmb;
+	}
+	
 	public void SetGrounded(bool grd){
 		this.grounded = grd;
+		
 	}
-
+	
 	public int GetDirection(){
 		return this.direction;
 	}
-
+	
 	public string GetTag(){
 		return this.tag.ToString();
 	}
-
+	
 	public Vector3 GetPosition(){
 		return this.transform.position;
 	}
-
-
+	
+	public bool GetCanClimb(){
+		return this.canClimb;
+	}
+	
 }
 
 
