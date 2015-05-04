64db767d1b6533527e75e95afe87ac936aa1a6fa
Sam-Browder <browdess@rose-hulman.edu>
2015-05-04 17:10:32 -0400

    added more ground prefabs

--- Robot Number II/Assets/Scripts/Attack/JetPack.cs
+++ Robot Number II/Assets/Scripts/Attack/JetPack.cs
@@ -43,6 +43,7 @@ public class JetPack : IAbility {
 	
 	public void ApplyAbility(ICharacter character){
 		Rigidbody2D rb2d = character.GetRB2D ();
+		rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
 		rb2d.AddForce(Vector2.up * this.jumpPower);	
 		
 		for (int i = 0; i < effects.Count; i++) {
