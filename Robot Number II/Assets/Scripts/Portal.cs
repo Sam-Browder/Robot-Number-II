80d5fea02913af52fd53a046a5180ce4a2fd4def
Sam-Browder <browdess@rose-hulman.edu>
2015-05-13 19:37:24 -0400

    working on portal physics

--- Robot Number II/Assets/Scripts/Portal.cs
+++ Robot Number II/Assets/Scripts/Portal.cs
@@ -7,6 +7,8 @@ public class Portal : MonoBehaviour {
 	private Vector3 newPos;
 	public GameObject destPortal;
 	private int direction;
+	private Vector2 oldVeloctiy;
+	private Vector2 newVelocity;
 	// Use this for initialization
 	void Start () {
 
@@ -19,6 +21,8 @@ public class Portal : MonoBehaviour {
 		this.newPos = new Vector3 (destPortal.transform.position.x + (this.direction* 1f),
 		                      destPortal.transform.position.y,
 		                      destPortal.transform.position.z);
+
+
 		//this.newPos = this.destPortal.transform.position;
 	}
 	
@@ -30,10 +34,24 @@ public class Portal : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		if (other.gameObject.CompareTag ("Player")) {
-			
+			//this.oldVeloctiy = other.attachedRigidbody.velocity;
+
+			//Vector3 temp = new Vector3(this.oldVeloctiy.x, this.oldVeloctiy.y, 0);
+
+
+			//Quaternion quat = Quaternion.AngleAxis(this.destPortal.transform.rotation.z, Vector3.forward);
+
+
+
+			//temp = temp*quat;
+
+			//this.newVelocity = new Vector2(temp.x,temp.y);
+			 
 		
 			//other.transform.position.Set(newPos.x, newPos.y, newPos.y);
 			other.transform.position = newPos;
+			//other.attachedRigidbody.velocity.Set(this.newVelocity.x, this.newVelocity.y);
+
 		}
 		
 		
