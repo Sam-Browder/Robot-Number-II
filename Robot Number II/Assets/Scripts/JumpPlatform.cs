774216393ff97bdda383ba1b25fada6e47221a6e
Sam-Browder <browdess@rose-hulman.edu>
2015-05-15 13:09:36 -0400

    fix

--- Robot Number II/Assets/Scripts/JumpPlatform.cs
+++ Robot Number II/Assets/Scripts/JumpPlatform.cs
@@ -20,6 +20,7 @@ public class JumpPlatform : MonoBehaviour {
 		if (other.gameObject.CompareTag ("Player")) {
 
 			//other.attachedRigidbody.AddForce(Vector2.up * this.jumpPower);
+			other.SendMessage("SetYVelocity",0.0);
 			other.SendMessage("SuperJump", this.jumpPower);
 		}
 		
