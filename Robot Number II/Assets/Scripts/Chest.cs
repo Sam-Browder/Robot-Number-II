e896ed4912e2d7d6ddde60db809ecc83f12d012f
Sam-Browder <browdess@rose-hulman.edu>
2015-05-17 14:22:06 -0400

    Revert "Revert "Chests added""
    This reverts commit 738db495b237c561aeda26c8ed71c011f864652e.

---
+++ Robot Number II/Assets/Scripts/Chest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Chest : MonoBehaviour
+{
+	
+	private bool wasTouched;
+	private Animator anim;
+	private TestMenu testMenu;
+
+	public int goldAmount;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		this.wasTouched = false;
+		anim = gameObject.GetComponent<Animator> ();
+		this.testMenu = GameObject.FindObjectOfType<TestMenu> ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		anim.SetBool ("wasTouched", this.wasTouched);
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Player")) {
+			this.wasTouched = true;
+			this.testMenu.money += goldAmount;
+
+		}
+	}
+}
