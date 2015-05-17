db02e349ff4645dedee9932f1c2557f6de017406
unknown <localmgr@HARRISSA-W520.rose-hulman.edu>
2015-05-17 04:34:33 -0400

    door works.

---
+++ Robot Number II/Assets/Scripts/LevelTools/Door.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Door : MonoBehaviour
+{
+
+	public EnemyController enemy;
+	private Animator anim;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		this.anim = gameObject.GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Physics2D.IgnoreLayerCollision (LayerMask.NameToLayer ("Player"), LayerMask.NameToLayer ("Door"), enemy == null);
+		Physics2D.IgnoreLayerCollision (LayerMask.NameToLayer ("Enemy"), LayerMask.NameToLayer ("Door"), enemy == null);
+
+		this.anim.SetBool ("enemyKilled", enemy == null);
+	}
+
+}
